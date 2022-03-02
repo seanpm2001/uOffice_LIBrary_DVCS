9cd6ab3e6887caa5ca719fa4f2ba56398f32fb05
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-03-01 16:25:01 -0800

    MAIN.py [V1]
    Version 1 of the main script for this directory set.

---
+++ Prompt/MAIN.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# Start of script
+# The prompt for using the DVCS version control system in uOffice.
+# Import section
+import os
+import pysimplegui
+import random
+import uoffice
+# Class section
+class uOffice_LIB_DVCS_Prompt_Main():
+  def promptString():
+    print("Version control")
+    print("[Save new version] [View version history] [Revert to previous version]")
+    print("<div>$Versions</div>")
+    print("[Close] [Delete] [Rubbish bin]")
+    break
+  def saveNewVersion():
+    # Coming soon
+    '''
+    Save New Version Info
+    Creates a new revision based on the latest save of the document
+    '''
+    break
+  def viewVersionHistory():
+    # Coming soon
+    '''
+    View version history Info
+    View the revision history in detail: to the side: shows size, encoding, formatting, languages, word count, character count, page count, etc.
+    '''
+    break
+  def revertToPreviousVersion():
+    # Coming soon
+    '''
+    Revert to Previous Version Info
+    Reverts to the latest revision, keeps a copy of all successor revisions for backup
+    '''
+    break
+  def closePrompt():
+    # Coming soon
+    '''
+    Close prompt Info
+    Closes the entire prompt/window, but not uOffice
+    '''
+    break
+  def deleteRevision():
+    # Coming soon
+    '''
+    Delete Revision Info
+    Deletes the selected revision(s)
+    '''
+    break
+  def rubbishBinView():
+    # Coming soon
+    '''
+    View Rubbish Bin Info
+    Look through deleted revisions, and chose to restore them, or permanently delete them.
+    '''
+    break
+# Execution layer section 
+return uOffice_LIB_DVCS_Prompt_Main()
+noMore = input("Press [ENTER] key to quit")
+break
+"""
+File info
+# File type: Python source file (*py)
+# File version: 1 (2022 Tuesday, March 1st at 4:25 pm)
+# Line count (including blank lines and compiler line): 70
+"""
+# End of script
